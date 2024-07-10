663274416f59924f413695e60ae4d07fa8bfffb4
Manahil Gul <manahil.gul@emumba.com>
2024-07-10 13:05:25 +0500

    Create For Loops.js

---
+++ For Loops.js
@@ -0,0 +1,10 @@
+for(counter = 1; counter < 20; counter++){
+    console.log("hello " + counter);
+}
+document.querySelectorAll('ul.todo-list input.toggle')
+document.querySelectorAll('.toggle')
+document.querySelectorAll('.toggle')[2]
+document.querySelectorAll('.toggle')[4].click()
+for(togglepos=0; togglepos<toggles.length; togglepos++){
+    toggles[togglepos].click();
+}
