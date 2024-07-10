c9fe4740ddd705454abb06f77d21656b0b516b41
Manahil Gul <manahil.gul@emumba.com>
2024-07-10 14:22:38 +0500

    Create bot pt.2.js

---
+++ bot pt.2.js
@@ -0,0 +1,118 @@
+this.createRandomTodo = function(){
+    console.log("create todo");   
+    autoTodo.createTodo("random todo " +  Date.now());
+}
+this.amendRandomTodo = function(){
+    x = getRandomItemIndex();
+    if(x>0){  
+        console.log("amend todo" + x);   
+        autoTodo.amendTodo(x, "amended random todo " +  Date.now());
+    }
+}
+var theFunctions = [];
+
+for(var prop in rando){
+    if(typeof rando[prop]== 'function'){
+        theFunctions.push(prop)
+    }
+}
+var randomFunctionIndex = Math.floor(Math.random()*theFunctions.length);
+var theFunctions = [];
+
+for(var prop in rando){
+    if(typeof rando[prop]== 'function'){
+        theFunctions.push(prop)
+    }
+}
+
+var randomFunctionIndex = Math.floor(Math.random()*theFunctions.length);
+
+rando[theFunctions[randomFunctionIndex]]();
+var randoBot = setInterval(function(){
+
+	var theFunctions = [];
+
+	for(var prop in rando){
+        if(typeof rando[prop]== 'function'){
+            theFunctions.push(prop)
+        }
+	}
+
+	var randomFunctionIndex = Math.floor(Math.random()*theFunctions.length);
+
+	rando[theFunctions[randomFunctionIndex]]();
+
+},1000);
+
+//clearInterval(randoBot)
+var rando = new function(){
+
+    function getRandomInt(x){
+        return Math.floor(Math.random() * x);
+    }
+
+    function getRandomItemIndex(){
+        max = document.querySelectorAll("ul.todo-list li").length;
+        if(max===0){
+            console.log("no items to choose from");
+            return 0;
+        }
+        x = getRandomInt(max)+1;
+        return x;
+    }
+
+    this.toggleAll = function(){
+        console.log("toggle all");
+        autoTodo.toggleAll();
+    }
+
+    this.selectRandomItem = function () {    
+        x = getRandomItemIndex();
+        if(x>0){    
+            console.log("select item " + x);
+            autoTodo.selectItemX(x);
+        }
+    }
+
+    this.deleteRandomItem = function () {        
+        x = getRandomItemIndex();
+        if(x>0){    
+            console.log("delete item " + x);
+            autoTodo.deleteItemX(x);
+        }
+    }
+
+    this.clearCompleted = function () {    
+        console.log("clear completed");   
+        autoTodo.clearCompleted();
+    }
+
+    this.filterAll = function () {   
+        console.log("filter all");        
+        autoTodo.filterAll();
+    }
+
+    this.filterActive = function () {        
+        console.log("filter active");   
+        autoTodo.filterActive();
+    }
+
+    this.filterCompleted = function () {      
+        console.log("filter completed");     
+        autoTodo.filterCompleted();
+    }
+
+    this.createRandomTodo = function(){
+        console.log("create todo");   
+        autoTodo.createTodo("random todo " +  Date.now());
+    }
+
+    this.amendRandomTodo = function(){
+        x = getRandomItemIndex();
+        if(x>0){  
+            console.log("amend todo" + x);   
+            autoTodo.amendTodo(x, "amended random todo " +  Date.now());
+        }
+    }
+}
+
