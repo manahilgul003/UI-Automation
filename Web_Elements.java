66f5811c9cc728fc6839464fd6decadc268dacc6
Manahil Gul <manahil.gul@emumba.com>
2024-07-10 15:30:47 +0500

    Create Web_Elements.java

---
+++ Web_Elements.java
@@ -0,0 +1,29 @@
+package base;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.List;
+public class BaseTests {
+    private WebDriver driver;
+    public void setup(){
+        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
+        driver=new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        System.out.println(links.size());
+        WebElement inputsLink =driver.findElement(By.linkText("Inputs"));
+        inputsLink.click();
+
+        System.out.println(driver.getTitle());
+
+
+    }
+public static void main(String [] args){
+        BaseTests test=new BaseTests();
+        test.setup();
+
+}
+}
