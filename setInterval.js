8941855f913ade693019ec7ac18501c6df923980
Manahil Gul <manahil.gul@emumba.com>
2024-07-10 14:17:06 +0500

    Create setInterval.js

---
+++ setInterval.js
@@ -0,0 +1,13 @@
+var sayHello = setInterval(function(){console.log("hello")},1000);
+var botTodoCount = 0;
+var creatorBot = setInterval(
+    function(){
+        document.querySelector('input.new-todo').value="todo " +botTodoCount;
+        document.querySelector('input.new-todo').dispatchEvent(new Event('change',{'bubbles':true}));
+
+        botTodoCount++;
+
+        if(botTodoCount>=100){
+            clearInterval(creatorBot);
+        }
+    }, 500);
