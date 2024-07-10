a4c6141e54f2c5a974efd28a3661325a228f0670
Manahil Gul <manahil.gul@emumba.com>
2024-07-10 13:07:12 +0500

    Create if Statements.js

---
+++ if Statements.js
@@ -0,0 +1,28 @@
+if(true){
+    console.log("hello");
+}
+if(false){
+    console.log("hello");
+}
+if(1===1){
+    console.log("hello");
+}
+if(1==1){
+    console.log("hello");
+}
+if(1==1){
+    console.log("hello");
+}
+if(1===1){
+    console.log("hello");
+}
+var doit=true;
+if(doit){
+    console.log("done");
+}
+var doit=false;
+if(doit){
+    console.log("done");
+}
+5%3 
+ 6%3 
