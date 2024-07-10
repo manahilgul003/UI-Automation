907af77ee5d629b1bd31bb819a14153242d826cc
Manahil Gul <manahil.gul@emumba.com>
2024-07-10 13:08:26 +0500

    Update if Statements.js

--- if Statements.js
+++ if Statements.js
@@ -25,4 +25,42 @@ if(doit){
     console.log("done");
 }
 5%3 
- 6%3 
+6%3 
+var toggles = document.querySelectorAll('.toggle');
+for(togglepos=0;togglepos<toggles.length;togglepos++){
+	if(togglepos%2==0){
+		toggles[togglepos].click();
+	}
+}
+// select every second item in the list
+var toggles = document.querySelectorAll('.toggle');
+for(togglepos=0;togglepos<toggles.length;togglepos++){
+	if(togglepos%2==1){
+		toggles[togglepos].click();
+	}
+}
+var toggles = document.querySelectorAll(".toggle");
+for(togglepos=0; togglepos<toggles.length; togglepos+=2){
+    toggles[togglepos].click();
+}
+var toggles = document.querySelectorAll(".toggle");
+for(togglepos=1; togglepos<toggles.length; togglepos+=2){
+        toggles[togglepos].click();
+}
+var toggles = document.querySelectorAll(".toggle");
+for(togglepos=1; togglepos<toggles.length; togglepos= togglepos+2){
+        toggles[togglepos].click();
+}
+var toggles = document.querySelectorAll(".toggle");
+var toggleit = false;
+for(togglepos=0; togglepos<toggles.length; togglepos++){
+    if(toggleit){
+        toggles[togglepos].click();
+    }
+    toggleit=!toggleit;
+}
+if(true){
+    console.log("true it is");
+} else {
+    console.log("false indeed");
+}
