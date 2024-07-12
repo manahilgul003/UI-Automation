70885ff932385bea2cc3098d46437c5998d15d26
Manahil Gul <manahil.gul@emumba.com>
2024-07-12 10:07:44 +0500

    Update and rename FLuent_way.java to HomePage(Fluent-way).java

--- FLuent_way.java
+++
@@ -1,8 +0,0 @@
-public String getNestedText() {
-	 WebElement nestedText = getDriver ().findElement (By.id ("shadow_host"))
-		 .getShadowRoot ()
-		 .findElement (By.cssSelector ("#nested_shadow_host"))
-		 .getShadowRoot ()
-		 .findElement (By.cssSelector ("#nested_shadow_content > div"));
-	 return nestedText.getText ();
-	}
