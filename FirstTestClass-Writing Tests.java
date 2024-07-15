1881c7ba8818f1eea55809ad0446dc60f4fd56bd
Manahil Gul <manahil.gul@emumba.com>
2024-07-15 10:22:03 +0500

    Create FirstTestClass-Writing Tests.java

---
+++ FirstTestClass-Writing Tests.java
@@ -0,0 +1,16 @@
+package junit5tests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class FirstTestClass {
+    @Test
+    void  firstMethod(){
+        System.out.println("This is the first test method");
+    }
+@Test
+@DisplayName("Manahil Gul-TC12-this method is the second one")
+    void secondMethod() {
+    System.out.println("This is the second test Method");
+}
+}
