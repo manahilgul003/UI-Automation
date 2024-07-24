c84f1ec3203babeb716808ba3755e654e487f4c0
Manahil Gul <manahil.gul@emumba.com>
2024-07-24 11:23:31 +0500

    Create Steps_DataTable.java

---
+++ Steps_DataTable.java
@@ -0,0 +1,47 @@
+package com.tau.steps;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Steps_DataTable {
+	private WebDriver mydriver;
+
+
+	@Given("I am in the login page of Sauce Demo Application")
+	public void i_am_in_the_login_page_of_Sauce_Demo_Application() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ayesha Noor\\eclipse-workspace\\tau-tester-course\\src\\test\\resources\\chromedriver.exe");
+		mydriver=new ChromeDriver();
+	  mydriver.get("https://www.saucedemo.com/");
+	}
+	@When("I enter valid credentials")
+	public void i_enter_valid_credentials(io.cucumber.datatable.DataTable table) {
+		List<String> loginForm=table.asList();
+		WebElement username1 = mydriver.findElement(By.id("user-name"));
+        WebElement password1 = mydriver.findElement(By.id("password"));
+        username1.sendKeys(loginForm.get(0));
+        password1.sendKeys(loginForm.get(1));
+        WebElement loginButton = mydriver.findElement(By.id("login-button"));
+      loginButton.click();
+	   
+	}
+
+	@Then("I should be taken to the Product Page")
+	public void i_should_be_taken_to_the_Product_Page() {
+		mydriver.findElement(By.className("header_secondary_container")).isDisplayed();
+		
+	}
+
+
+
+	
+
+}
